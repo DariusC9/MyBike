b4b2769335e8c1ade3010ba310b412068cce1154
Couti Darius <coutidarius@gmail.com>
2023-05-31 13:16:55 +0300

    Finished the EmptyBikeView

---
+++ MyBike/MyBike/EmptyBikeView.swift
@@ -0,0 +1,84 @@
+//
+//  EmptyBikeView.swift
+//  MyBike
+//
+//  Created by Darius Couti on 31.05.2023.
+//
+
+import SwiftUI
+
+struct EmptyBikeView: View {
+    var body: some View {
+        
+        VStack(spacing: 0){
+                Image("missing_bike_card")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 5)
+                    
+            
+            
+                ZStack(alignment: .leading) {
+                    Image("dotted_line")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 20)
+//                        .overlay(
+//                                    Text("""
+//                                        You don't have any bike added yet.
+//                                        Add your first bike and start register
+//                                        your rides in order to keep your bike
+//                                        service cycles up to date.
+//                                        """)
+//                                        .multilineTextAlignment(.center)
+//                                        .padding(.horizontal, 20)
+//                                        .background(Color.black)
+//                                        .opacity(1)
+//                                        .foregroundColor(.white)
+//                                        .cornerRadius(10)
+//                                    )
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Text("""
+        You don't have any bike added yet.
+        Add your first bike and start register
+        your rides in order to keep your bike
+        service cycles up to date.
+        """)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .padding()
+                            Spacer()
+                        }
+                        .background(Color.black.opacity(1))
+                        .padding()
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Text("Add Car")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .padding(.horizontal, 5)
+                                .frame(maxWidth: .infinity, minHeight: 44)
+                                .background(Color.blue)
+                                .cornerRadius(5)
+                        }
+                        .padding(.horizontal, 5)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+}
+
+struct EmptyBikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyBikeView()
+    }
+}
+
