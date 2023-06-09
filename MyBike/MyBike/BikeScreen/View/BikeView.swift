5b8f89c1824b3ff0a02e947efaef9b84f0118c71
Couti Darius <coutidarius@gmail.com>
2023-06-09 16:49:55 +0300

    Fixed UI bugs on items in list

--- MyBike/MyBike/BikeScreen/View/BikeView.swift
+++ MyBike/MyBike/BikeScreen/View/BikeView.swift
@@ -14,7 +14,7 @@ struct BikeView: View {
     @State private var showingAlert = false
     @State private var selectedBike: BikeModel? = nil
     @State private var bikeID: UUID = UUID()
-
+    
     @State var rides = PersistenceController.shared.fetchRides()
     @State var bikes = PersistenceController.shared.fetchBikes()
     
@@ -24,52 +24,58 @@ struct BikeView: View {
         
         VStack {
             List(bikeList, id: \.self) { item in
-                NavigationLink(destination: BikeDetailsView(model: item, allRides: Transformer.shared.convertToRideModel(from: rides))) {
-                    ZStack(alignment: .topTrailing) {
-                        BikeCell(model: item)
-                            .padding(.top, 15)
-                        Menu {
-                            Button {
-                                selectedBike = item
-                                goToEditBikeView.toggle()
-                            } label: {
+                ZStack {
+                    BikeCell(model: item)
+                    NavigationLink(destination: BikeDetailsView(model: item, allRides: Transformer.shared.convertToRideModel(from: rides))) {
+                        EmptyView()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Menu {
+                                Button {
+                                    selectedBike = item
+                                    goToEditBikeView.toggle()
+                                } label: {
                                     Image("icon_edit")
                                     Text("Edit")
-                            }
-                            Button {
-                                showingAlert.toggle()
-                                bikeID = item.ID
-
-                            } label: {
+                                }
+                                Button {
+                                    showingAlert.toggle()
+                                    bikeID = item.ID
+                                    
+                                } label: {
                                     Text("Delete")
                                     Image("icon_delete")
+                                }
+                                
+                            } label: {
+                                Image("icon_overflow")
                             }
-
-                        } label: {
-                            Image("icon_overflow")
+                            .padding()
+                            .alert("\(Transformer.shared.fetchBikeName(bikeID)) will be deleted", isPresented: $showingAlert) {
+                                Button("Cancel", role: .cancel) { }
+                                Button("Delete", role: .destructive) {
+                                    guard let bike = Transformer.shared.fetchBike(from: bikeID) else { return}
+                                    PersistenceController.shared.deleteBike(bike)
+                                }
+                            }
                         }
-                        .padding()
-                        .alert("\(Transformer.shared.fetchBikeName(bikeID)) will be deleted", isPresented: $showingAlert) {
-                            Button("Cancel", role: .cancel) { }
-                            Button("Delete", role: .destructive) {
-                                guard let bike = Transformer.shared.fetchBike(from: bikeID) else { return}
-                                PersistenceController.shared.deleteBike(bike)
-                            }
+                        Spacer()
                     }
-                        NavigationLink(destination: EditBikeView(defaultBike: Transformer.shared.defaultBikeBool(from: selectedBike ?? item, bikes), selectedBike: selectedBike ?? item), isActive: $goToEditBikeView) {
-                            EmptyView()
-                                    }
-                        .hidden()
+                    NavigationLink(destination: EditBikeView(defaultBike: Transformer.shared.defaultBikeBool(from: selectedBike ?? item, bikes), selectedBike: selectedBike ?? item), isActive: $goToEditBikeView) {
+                        EmptyView()
                     }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .buttonStyle(PlainButtonStyle())
+                    .hidden()
                 }
                 .listRowInsets(EdgeInsets())
+                .padding(.top, 10)
+                .background(.black)
             }
-            .scrollContentBackground(.hidden)
         }
-        .background(.black)
+        .scrollContentBackground(.hidden)
+        .listStyle(.plain)
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(
             leading: NavigationLink(destination: AddBikeView(), isActive: $goToAddBikeView) {
